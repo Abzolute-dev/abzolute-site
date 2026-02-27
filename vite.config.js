4c38161e9c335dde29f8146a84c1a89c3f77d65d
KB Cars Reading <info@kbcarsreading.co.uk>
2026-02-27 20:35:21 +0000

    Initial commit of Abzolute Studio website

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // Add this
 
 export default defineConfig({
+  base: "/abzolute-site/",
   plugins: [
     react(),
     tailwindcss(), // Add this
